eb3b1a38a9724dfc3327efe6cf76e52ff7fd24ae
greenpandastudios <augustamiller77@gmail.com>
2025-10-05 11:21:25 -0500

    Fix build warnings

--- src/components/EyeTalkApp.tsx
+++ src/components/EyeTalkApp.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useEyeTracking } from '../hooks/useEyeTracking';
 import { useOpenAI } from '../hooks/useOpenAI';
 import { PhoneticWheel } from './PhoneticWheel';
